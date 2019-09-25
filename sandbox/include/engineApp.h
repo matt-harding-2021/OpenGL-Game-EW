71b39d0cb5dc4d6c0f21c9987ac50ca5fcebe835
simonCoupland <simon.coupland@gmail.com>
2019-09-25 09:02:28 +0100

    Add basic sandbox files

---
+++ sandbox/include/engineApp.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Engine.h>
+
+class engineApp : public Engine::Application
+{
+public:
+	engineApp();
+	~engineApp() override;
+};
---
+++ sandbox/src/engineApp.cpp
@@ -0,0 +1,21 @@
+/** \file engineApp.cpp
+*/
+#include "engineApp.h"
+
+
+engineApp::engineApp()
+{
+}
+
+engineApp::~engineApp()
+{
+
+}
+
+
+
+Engine::Application* Engine::startApplication()
+{
+
+	return new engineApp();
+}
